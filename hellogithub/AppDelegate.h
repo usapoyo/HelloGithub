cd197160234955131675ec59902bf2eff41e9e6c
usapoyo <usapoyo@gmail.com>
2013-08-13 02:13:52 +0900

    Initial Commit

---
+++ hellogithub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hellogithub
+//
+//  Created by usapoyo on 2013/08/13.
+//  Copyright (c) 2013年 usapoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hellogithub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hellogithub
+//
+//  Created by usapoyo on 2013/08/13.
+//  Copyright (c) 2013年 usapoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
